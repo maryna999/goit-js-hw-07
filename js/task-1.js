e8a0d92c9d664ce108d4d486938e3a5a6392024d
Maryna <mishka999@ukr.net>
2024-06-29 16:29:12 +0300

    add changed tasks

--- js/task-1.js
+++ js/task-1.js
@@ -8,16 +8,9 @@ const categories = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(category => {
-    const categoryTitle = document.querySelector("h2").textContent;
+    const categoryTitle = category.querySelector("h2").textContent;
     console.log(`Category: ${categoryTitle}`);
 
     const totalCategoriesItems = category.querySelectorAll("ul li").length;
     console.log(`Elements: ${totalCategoriesItems}`);
     });
-
-
-
-
-
-
-
